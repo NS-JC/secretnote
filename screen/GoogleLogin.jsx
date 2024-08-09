981c0824d731cd5ce696bbce7acb470bc5b69648
Namsik Cho <chonamsik1@gmail.com>
2024-08-09 16:06:00 +0700

    trying to change build.gradle

--- screen/GoogleLogin.jsx
+++ screen/GoogleLogin.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
-import { View, Pressable, TouchableOpacity, Text, StyleSheet, Image} from 'react-native';
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+import { View, TouchableOpacity, Text, StyleSheet, Image} from 'react-native';
 import { GoogleSignin } from '@react-native-google-signin/google-signin';
 import {
 	GOOGLE_WEB_CLIENT_ID,
@@ -50,18 +51,59 @@ export default function App() {
 
 	return (
 		<View style={styles.container}>
-			<Pressable onPress={handleGoogleLogin}><Text>Continue with Google</Text></Pressable>
-			<Image style={{height:50, width:50}} resizeMode='cover' source={{uri:user?.photo}}></Image>
-			<Text>{user?.name}</Text>
+			<Text style={styles.title}>The Secret Note</Text>
+			<TouchableOpacity style={styles.button} onPress={handleGoogleLogin}>
+				<Image source={require('../src/GoogleIcon.png')} style={styles.icon} />
+				<Text style={styles.buttonText}>구글 아이디로 로그인하기</Text>
+			</TouchableOpacity>
+			{user && (
+				<>
+					<Image
+						style={{ height: 50, width: 50 }}
+						resizeMode='cover'
+						source={{ uri: user?.photo }}
+					/>
+					<Text>{user?.name}</Text>
+				</>
+			)}
 		</View>
 	);
 }
 
 const styles = StyleSheet.create({
 	container: {
-	  flex: 1,
-	  justifyContent: 'center',
-	  alignItems: 'center',
-	  backgroundColor: '#f0f0f5',
-	}
-})
+		flex: 1,
+		justifyContent: 'flex-end',
+		alignItems: 'center',
+		backgroundColor: '#f0f0f5',
+		paddingBottom: hp('15%'),  // 15% of the screen height
+		position: 'relative',
+	},
+	title: {
+		fontSize: wp('8%'),  // Font size based on 8% of the screen width
+		fontWeight: 'bold',
+		position: 'absolute',
+		top: hp('40%'),  // 40% from the top of the screen height
+		textAlign: 'center',
+		width: '100%',
+	},
+	button: {
+		flexDirection: 'row',
+		alignItems: 'center',
+		backgroundColor: '#fff',
+		paddingVertical: hp('1.5%'),  // Padding vertical based on 1.5% of the screen height
+		paddingHorizontal: wp('7%'),  // Padding horizontal based on 7% of the screen width
+		borderRadius: 5,
+		borderWidth: 1,
+		borderColor: '#ccc',
+	},
+	icon: {
+		width: wp('6%'),  // Icon width based on 6% of the screen width
+		height: wp('6%'),  // Icon height based on 6% of the screen width
+		marginRight: wp('2.5%'),  // Margin right based on 2.5% of the screen width
+	},
+	buttonText: {
+		fontSize: wp('4%'),  // Font size based on 4% of the screen width
+		color: '#333',
+	},
+});
